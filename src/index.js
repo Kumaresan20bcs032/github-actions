cb0d71ec2b93a0c4ad02b8a2ecb614f0446bebba
KumaresanR-sparkout <kumaresan@sparkouttech.com>
2024-09-22 18:03:46 +0530

    index file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Application api working" });
+})
+
+
+const PORT = process.env?.PORT ?? 3000;
+app.listen(PORT, () => {
+    console.log(`Server started at host http://localhost:${PORT}`);
+})
